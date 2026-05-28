b44655da999334c8ccf26697ddf807c9a0d36fd5
unknown <pedro.avare01@gmail.com>
2026-05-28 11:18:28 -0300

    fixes basename url

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,7 +10,7 @@ const AppRoutes = () => {
   useSetDailyDiscountPackage();
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/poke-cards'>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<HomePage />} />
